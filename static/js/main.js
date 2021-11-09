699ba822b6b88509d0cbf20547719559a667ffb2
Emanuele <emanuele.micciulla@gmail.com>
2021-11-09 17:12:19 +0100

    fix the random js

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,8 @@
-{
+(function (){
+    const formatDate = date => date.toISOString().replace(/^(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})(\.\d+)?Z$/,'$1 $2');
     const display = document.querySelector("#display");
-    const loop = setInterval(p=>{
-        const now = new Date();
-        console.log('p',p,now.toISOString());
-        display.textContent=now.toISOString();
+    const loop = setInterval(_=>{
+        display.textContent=formatDate(new Date());
     },1000);
-}
+    console.log("loop",loop);
+}())
